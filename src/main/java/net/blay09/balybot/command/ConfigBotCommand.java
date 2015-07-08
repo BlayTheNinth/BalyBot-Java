4ae017556bb3b3ffb7c1e58fb21fd499909e2519
Blay09 <blay09@hotmail.de>
2015-07-09 01:12:39 +0200

    Fix config command permissions.

--- src/main/java/net/blay09/balybot/command/ConfigBotCommand.java
+++ src/main/java/net/blay09/balybot/command/ConfigBotCommand.java
@@ -1,6 +1,5 @@
 package net.blay09.balybot.command;
 
-import net.blay09.balybot.BalyBot;
 import net.blay09.balybot.Config;
 import net.blay09.balybot.UserLevel;
 import net.blay09.balybot.irc.IRCChannel;
@@ -23,14 +22,14 @@ public class ConfigBotCommand extends BotCommand {
 
         String channelName, option, value;
         if(args.length >= 3) {
-            channelName = args[0];
-            option = args[1];
-            value = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
-        } else {
             if(!CommandHandler.passesUserLevel(sender, channel, UserLevel.OWNER)) {
                 channel.message("Global options and configurations of other channels can only be changed as bot owner.");
                 return;
             }
+            channelName = args[0];
+            option = args[1];
+            value = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+        } else {
             channelName = channel.getName();
             option = args[0];
             value = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
