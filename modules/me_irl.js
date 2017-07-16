e585969d3b47bae3c2e39007a1c93f9278bfa045
Blay09 <blay09@hotmail.de>
2017-07-16 19:01:40 +0200

    me_irl

---
+++ modules/me_irl.js
@@ -0,0 +1,40 @@
+function module() {
+    return {
+        id: "me_irl",
+        name: "me_irl Module",
+        desc: "fuck_you_irl."
+    };
+}
+
+function configure() {
+    return [
+        {
+            name: "message",
+            value: "fuck_you_irl.",
+            desc: "im serious"
+        }
+    ];
+}
+
+function commands() {
+    return [
+
+    ]
+}
+
+function events() {
+    return {
+        "channel_chat": onChannelChat
+    };
+}
+
+/**
+ * @param channel : JChannel
+ * @param user : JUser
+ * @param message : JString
+ */
+function onChannelChat(channel, user, message) {
+    if(message.indexOf("me_irl") !== -1) {
+        JBalyBot.message(channel, config["message"]);
+    }
+}
